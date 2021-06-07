083c496d33c10079df9efc8d350131cc93bf80c3
Dmitriy Komar <dmitriyko@playtika.com>
2021-06-07 15:07:12 +0200

    feat: implement logic of task_4_p1

--- Swift_Task4/ios.stage-task/Exercises/Exercise-1/IntToRoman.swift
+++ Swift_Task4/ios.stage-task/Exercises/Exercise-1/IntToRoman.swift
@@ -3,6 +3,34 @@ import Foundation
 public extension Int {
     
     var roman: String? {
-        nil
+        
+        var integerValue = self
+        
+        if integerValue <= 0 {
+            return nil
+        } else {
+                var numeralString = ""
+                let mappingList: [(Int, String)] = [(1000, "M"),
+                                                    (900, "CM"),
+                                                    (500, "D"),
+                                                    (400, "CD"),
+                                                    (100, "C"),
+                                                    (90, "XC"),
+                                                    (50, "L"),
+                                                    (40, "XL"),
+                                                    (10, "X"),
+                                                    (9, "IX"),
+                                                    (5, "V"),
+                                                    (4, "IV"),
+                                                    (1, "I")]
+                for i in mappingList {
+                    while (integerValue >= i.0) {
+                        integerValue -= i.0
+                        numeralString += i.1
+                    }
+                }
+                return numeralString
+        }
+
     }
 }
