3e66e5bfd636ad7635b018f47e94d71d7325beae
Dmitriy Komar <dmitriyko@playtika.com>
2021-06-07 22:44:25 +0200

    fix: implement logic of task_4_p2

--- Swift_Task4/ios.stage-task/Exercises/Exercise-2/FillWithColor.swift
+++ Swift_Task4/ios.stage-task/Exercises/Exercise-2/FillWithColor.swift
@@ -3,6 +3,43 @@ import Foundation
 final class FillWithColor {
     
     func fillWithColor(_ image: [[Int]], _ row: Int, _ column: Int, _ newColor: Int) -> [[Int]] {
-        [[]]
+        
+                var newImage : [[Int]] = image
+                let initColor = image[row][column]
+                
+                var queue : [(Int, Int)] = [(row, column)]
+                
+                while !queue.isEmpty {
+                    
+                    let newCell = queue.removeFirst()
+                    newImage[newCell.0][newCell.1] = newColor
+                    
+                    if(newCell.0 + 2
+                        <= newImage.count
+                        && newImage[newCell.0 + 1][newCell.1]
+                        == initColor) {
+                        queue.append((newCell.0 + 1, newCell.1))
+                    }
+                    if(newCell.1 + 2
+                        <= newImage[0].count
+                        && newImage[newCell.0][newCell.1 + 1]
+                        == initColor) {
+                        queue.append((newCell.0, newCell.1 + 1))
+                    }
+                    if(newCell.0 - 1
+                        >= 0
+                        &&  newImage[newCell.0 - 1][newCell.1]
+                        == initColor) {
+                        queue.append((newCell.0 - 1, newCell.1))
+                    }
+                    if(newCell.1 - 1
+                        >= 0
+                        && newImage[newCell.0][newCell.1 - 1]
+                        == initColor) {
+                        queue.append((newCell.0, newCell.1 - 1))
+                    }
+                }
+                
+                return newImage
     }
 }
